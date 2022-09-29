fa557c1541610f9bb5dc8bc204fd44c49019800a
MaksLevin <maksim.levin@hiqo-solutions.com>
2022-09-29 12:15:09 +0300

    provide in app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { select, Store } from '@ngrx/store';
 
-import * as authActions from '@app/store/user-auth/user-auth.action';
-import * as authSelectors from '@app/store/user-auth/user-auth.selector';
-import { AuthService } from './core/services/auth.service';
-import { IUser } from './core/models/user';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { combineLatest, Observable, takeWhile, tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,22 +9,10 @@ import { combineLatest, Observable, takeWhile, tap } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'Trello-clone';
 
-  constructor(private store: Store, private auth: AngularFireAuth) {}
-  appIni(): Observable<any> {
-    return combineLatest([
-      this.auth.user ,
-      this.store.pipe(select(authSelectors.selectGetUserAuth)),
-    ]).pipe(
-      takeWhile(([user, dbUser]) => !!user?.uid && Object.keys(dbUser || {}).length === 0),
-      tap(([user, dbUser]) => {
-        if (user && Object.keys(dbUser || {}).length === 0) {
-          this.store.dispatch(authActions.getAuthUserSuccess(dbUser));
-        }
-      })
-    );
-  }
+  constructor() {}
+
 
   ngOnInit(): void {
-    this.appIni();
+
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,12 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { select, Store } from '@ngrx/store';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { combineLatest, Observable, takeWhile, tap } from 'rxjs';
 
+import * as authActions from '@app/store/user-auth/user-auth.action';
+import * as authSelectors from '@app/store/user-auth/user-auth.selector';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './auth/login/login.module';
@@ -20,6 +25,19 @@ import { RouterSerializer } from '@app/store/routerSerializer';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { HeaderModule } from './header/header.module';
 
+const APP_INITIALIZER = (store: Store, auth: AngularFireAuth): Observable<any>  => {
+  return combineLatest([
+    auth.user ,
+    store.pipe(select(authSelectors.selectGetUserAuth)),
+  ]).pipe(
+    takeWhile(([user, dbUser]) => !!user?.uid && Object.keys(dbUser || {}).length === 0),
+    tap(([user, dbUser]) => {
+      if (user && Object.keys(dbUser || {}).length === 0) {
+        store.dispatch(authActions.getAuthUserSuccess(dbUser));
+      }
+    })
+  );
+}
 @NgModule({
   declarations: [AppComponent],
   imports: [
@@ -43,7 +61,7 @@ import { HeaderModule } from './header/header.module';
     !environment.production ? StoreDevtoolsModule.instrument() : [],
   ],
   exports: [SharedModule],
-  providers: [],
+  providers: [{provide: APP_INITIALIZER, useFactory: APP_INITIALIZER}],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
