2eec85cce182bead4adbaf78aa47ddf8830a87da
MaksLevin <maksim.levin@hiqo-solutions.com>
2023-04-20 12:31:08 +0300

    Creating a pie chart

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from './statistics.component';
 import { BarComponent } from './bar/bar.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
+import { PieComponent } from './pie/pie.component';
 
 @NgModule({
-  declarations: [StatisticsComponent, BarComponent],
+  declarations: [StatisticsComponent, BarComponent, PieComponent],
   imports: [CommonModule, StatisticsRoutingModule],
 })
 export class StatisticsModule {}
