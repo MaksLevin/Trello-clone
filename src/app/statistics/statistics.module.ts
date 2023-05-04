5edfe4cfa66a12e403bf07658e57b4e6bf33b49c
MaksLevin <maksim.levin@hiqo-solutions.com>
2023-05-04 10:55:12 +0300

    created scatter plot

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from './statistics.component';
 import { BarComponent } from './bar/bar.component';
 import { StatisticsRoutingModule } from './statistics-routing.module';
+import { ScatterPlotComponent } from './scatter-plot/scatter-plot.component';
 
 @NgModule({
-  declarations: [StatisticsComponent, BarComponent],
+  declarations: [StatisticsComponent, BarComponent, ScatterPlotComponent],
   imports: [CommonModule, StatisticsRoutingModule],
 })
 export class StatisticsModule {}
